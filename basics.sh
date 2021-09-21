3b710a93553c33994c1ba85cde4f15a0f9d4611f
58851 <34036509+58851@users.noreply.github.com>
2021-09-21 00:38:57 -0400

    Update basics.sh

--- basics.sh
+++ basics.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+# CONFIGURE OS FIREWALL
 ufw allow 22
 ufw allow 80
 ufw allow 8080
@@ -9,15 +11,24 @@ ufw allow 8843
 ufw allow 3478
 ufw enable
 
+# INSTALL TIME SERVICE
 apt install ntp -y
+date
 
+# INSTALL RANDOM
 apt install haveged -y
 
+# CREATE AND CONFIGURE THE SWAPFILE
 fallocate -l 4G /swapfile
 chmod 600 /swapfile
 mkswap /swapfile
 swapon /swapfile
 sh -c 'echo "/swapfile none swap sw 0 0" >> /etc/fstab'
+cat /etc/fstab
 
-sh -c 'echo "apt autoremove -y" >> /etc/cron.monthly/autoremove'
-chmod +x /etc/cron.monthly/autoremove
+
+# AUTOMATICALLY REMOVE OBSOLETE PACKAGES
+#sh -c 'echo "apt autoremove -y" >> /etc/cron.monthly/autoremove'
+#chmod +x /etc/cron.monthly/autoremove
+
+dpkg-reconfigure tzdata
